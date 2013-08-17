e14a23dfe347d43478c3d1362e608e5a4d2342d2
Shinichiro Tanaka <shinichiro.tanaka.n81@gmail.com>
2013-08-17 06:33:00 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   var fs = requiqre('fs');
+   var str = fs.readFileSync('indext.html');
+   response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
